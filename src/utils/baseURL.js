e236d7339c040b13d45d20c025d0de6160a48e3c
Phongs4thon <plstr142@gmail.com>
2025-06-10 00:29:17 +0700

    update

--- src/utils/baseURL.js
+++ src/utils/baseURL.js
@@ -1,6 +1,9 @@
 // getBaseUrl
 const getBaseUrl = () => {
-  return "http://localhost:5000";
+  return (
+    // "http://localhost:5000" port รันเดิม
+    "https://st-bookinary-store-app-backend.vercel.app/"
+  );
 };
 
 export default getBaseUrl;
